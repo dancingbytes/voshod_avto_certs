4d007a2d5e4a1af3d5c024af09f8abea0cedd45b
Tyralion <piliaiev@gmail.com>
2017-09-26 17:12:51 +0500

    Правки

--- js/history.js
+++ js/history.js
@@ -27,7 +27,7 @@ if (!!(window.history && history.pushState)) {
   })();
 
   var updateCrumbs = function() {
-    window.document.title = window.location.pathname;
+
     setTimeout(function () {
       var loc = window.location.pathname;
       var segments = loc.split('/');
